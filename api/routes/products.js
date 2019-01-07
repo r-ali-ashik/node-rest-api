4b4830c9bfca0cccb8eac4c3c4c17cf304acdb42
aliashik <ramjan.ali.ashik@gmail.com>
2019-01-08 00:55:41 +0600

    adding product service

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,178 +1,21 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const multer = require('multer');
+
+
 const checkAuth = require('../middleware/check-auth');
+const productService = require('../service/productService');
 
 const router = express.Router();
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, new Date().toISOString() + file.originalname)
-    }
-});
-const fileFilter = function (req, file, cb) {
-    // reject a file cb(null, false);
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
-        cb(null, true);
-    } else {
-        cb(new Error('doesn\'t support ' + file.mimetype + ' type'), false);
-    }
-}
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 1024 * 1024 * 5
-    },
-    fileFilter: fileFilter
-});
+
 const Product = require('../models/product');
 
 router.route('/')
-    .get(function (req, res) {
-        Product.find()
-            .select('name price _id productImage')
-            .exec()
-            .then(docs => {
-                const response = {
-                    count: docs.length,
-                    products: docs.map(doc => {
-                        return {
-                            name: doc.name,
-                            price: doc.price,
-                            _id: doc._id,
-                            productImage: doc.productImage,
-                            request: {
-                                type: 'GET',
-                                url: 'http://localhost:3000/products/' + doc._id
-                            }
-                        }
-                    })
-                }
-                res.status(200).json(response);
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error: err
-                });
-            })
-    })
-    .post(checkAuth, upload.single('productImage'), function (req, res) {
-
-        const product = new Product({
-            _id: mongoose.Types.ObjectId(),
-            name: req.body.name,
-            price: req.body.price,
-            productImage: req.file.path
-        });
-
-        product.save().then(result => {
-            console.log(result);
-            res.status(201).json({
-                message: 'Created product successfully',
-                createdProduct: {
-                    name: result.name,
-                    price: result.price,
-                    _id: result._id,
-                    request: {
-                        type: 'GET',
-                        url: 'http://localhost:3000/products/' + result._id
-                    }
-                }
-            });
-        }).catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-    });
+    .get(productService.getProducts)
+    .post(checkAuth, productService.createProduct);
 
 router.route('/:productId')
-    .get(function (req, res) {
-        const id = req.params.productId;
-        Product.findById(id)
-            .select('name price _id productImage')
-            .exec()
-            .then(doc => {
-                console.log(doc);
-                if (doc) {
-                    res.status(200).json({
-                        product: doc,
-                        request: {
-                            type: 'GET',
-                            description: 'Get all products',
-                            url: 'http://localhost:3000/products'
-                        }
-                    });
-                } else {
-                    res.status(404).json({
-                        message: 'No data found for provided id'
-                    })
-                }
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error: err
-                })
-            });
-    })
-    .patch(checkAuth, function (req, res) {
-        const id = req.params.productId;
-        const updateOps = {};
-        for (const ops of req.body) {
-            updateOps[ops.name] = ops.value;
-        }
-        Product.update({
-                _id: id
-            }, {
-                $set: updateOps
-            })
-            .exec()
-            .then(result => {
-                res.status(200).json({
-                    message: 'Product updated',
-                    request: {
-                        type: 'GET',
-                        url: 'http://localhost:3000/products/' + id
-                    }
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error: err
-                })
-            });
-    })
-    .delete(checkAuth, function (req, res) {
-        Product.remove({
-                _id: req.params.productId
-            })
-            .exec()
-            .then(result => {
-                res.status(200).json({
-                    message: 'Product deleted',
-                    request: {
-                        type: 'POST',
-                        url: 'http://localhost:3000/products',
-                        body: {
-                            name: 'String',
-                            price: 'Number'
-                        }
-                    }
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error: err
-                })
-            });
-    });
+    .get(productService.getProduct)
+    .patch(checkAuth, productService.updateProduct)
+    .delete(checkAuth, productService.deleteProduct);
 
 
 module.exports = router;
