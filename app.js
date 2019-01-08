2bf5c4ada4180e0b0506b9f83ce7effbb58c3518
Ashik <ramjanali-dsi@gmail.com>
2019-01-08 15:37:36 +0600

    adding swagger

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+
+
 const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 const userRoute = require('./api/routes/users');
 
 mongoose.connect('mongodb://172.17.0.2:27017/test',  { useNewUrlParser: true });
+
+const swaggerRouter = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+swaggerRouter.use('/swagger', swaggerUi.serve);
+swaggerRouter.get('/swagger', swaggerUi.setup(swaggerDocument));
 
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
@@ -18,6 +27,8 @@ app.use(bodyParser.json());
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 app.use('/user', userRoute);
+app.use(swaggerRouter);
+
 
 app.use(function(req, res, next) {
     const error = new Error('Not found');
